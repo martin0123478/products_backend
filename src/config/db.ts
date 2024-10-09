c97fa85c7a3607db0030f8bc6f903cec5127701a
martin0123478 <martin_agui97@outlook.es>
2024-10-09 11:51:42 -0600

    feat:create model to data base

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,13 @@
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import dotenv from 'dotenv'
+import path from 'path';
+import { fileURLToPath } from 'url';
 dotenv.config()
-const db = new Sequelize(process.env.DATABASE_URL!)
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+const db = new Sequelize(process.env.DATABASE_URL!, {
+    models: [__dirname + '/../models/**/*.ts']
+})
 
 export default db
