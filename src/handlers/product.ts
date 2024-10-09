9c00d4a77c3d19a128001c44d61a134e099fd430
martin0123478 <martin_agui97@outlook.es>
2024-10-09 16:53:10 -0600

    feat:validate products

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from 'express'
 import Product from '../models/Product.model.js'
+import { validationResult } from 'express-validator'
 export const createProduct = async (req: Request, res: Response) => {
+    let erros = validationResult(req)
+    if (!erros.isEmpty()) {
+        return res.status(400).json({ erros: erros.array() })
+    }
     const product = await Product.create(req.body)
 
     res.json({ data: product })
--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,22 @@
 import { Router } from "express";
 import { createProduct } from "./handlers/product.js";
-
+import { body } from 'express-validator'
 const router = Router()
 
 router.get('/', (req, res) => {
     res.json('desde get')
 })
 
-router.post('/', createProduct)
+router.post('/',
+    //validacion
+    body('name')
+        .notEmpty().withMessage('El nombre del producto no puede ir vacio'),
+
+    body('price')
+        .isNumeric().withMessage('valor no valido')
+        .notEmpty().withMessage('El costo del producto no puede ir vacio')
+        .custom(value => value > 0).withMessage('precio no valido'),
+    createProduct)
 
 router.put('/', (req, res) => {
     res.json('desde put')
