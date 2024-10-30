33f1911e0970970b76631d58ffb6ed9dc8090b66
martin0123478 <martin_agui97@outlook.es>
2024-10-29 21:56:08 -0600

    feat:enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import colors from 'colors'
 import router from "./router.js";
+import cors, { CorsOptions } from 'cors'
 import db from "./config/db.js";
 //conectar a bd
 async function connctDB() {
@@ -16,6 +17,17 @@ async function connctDB() {
 }
 connctDB()
 const server = express()
+//cors
+const corsOptions: CorsOptions = {
+    origin: function (origin, callback) {
+        if (origin === process.env.FRONTEND_URL) {
+            callback(null, true)
+        } else {
+            callback(new Error('ERROR DE CORS'))
+        }
+    }
+}
+server.use(cors(corsOptions))
 server.use(express.json())
 server.use('/api/products', router)
 
